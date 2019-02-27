d95531e27c417eaaa09efad439ba921abfa189d1
Dev <dev@Delias-MacBook-Air.local>
2019-02-27 10:53:11 +0000

    forms added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,48 @@
-<!-- <template>
-  <section>
-    <game-form />
-    <games-grid />
-  </section>
+<template>
+  <div id="app">
+    <bookings-form />
+    <bookings-grid :bookings="bookings" />
+  </div>
 </template>
 
 <script>
-import GameForm from './components/GameForm.vue';
-import GamesGrid from './components/GamesGrid.vue';
+import BookingsForm from './components/BookingsForm.vue';
+import BookingsGrid from './components/BookingsGrid.vue';
+import { eventBus } from './main.js';
 
 export default {
   name: 'app',
+  data () {
+    return {
+      bookings: []
+    }
+  },
   components: {
-    GamesGrid,
-    GameForm
+    BookingsForm,
+    BookingsGrid
+  },
+  mounted(){
+    this.fetchData();
+
+    eventBus.$on('booking-added', booking =>
+    this.bookings.push(booking));
+
+    eventBus.$on('booking-deleted', id =>
+    this.deleteBooking(id));
+},
+methods: {
+  fetchData(){
+    fetch("http://localhost:3000/api/bookings")
+    .then(res => res.json())
+    .then(bookings => this.bookings = bookings);
+  },
+  deleteBooking(id){
+    const index = this.bookings.findIndex((booking) => {
+      return booking._id === id
+    });
+    this.booking.splice(index, 1);
   }
+}
 }
 </script>
 
@@ -22,14 +50,13 @@ export default {
 
 body {
   font-family: 'Source Serif Pro', serif;
-  background-color: #e2dee8;
   width: 80%;
   margin: 0 auto;
-  color: #0E0B16;
+  /* color: #0E0B16; */
 }
 
 h1, h2, h3 {
-  color: #A239CA;
-  font-family: 'Skranji', cursive;
+  /* color: #A239CA; */
+  /* font-family: 'Skranji', cursive; */
 }
-</style> -->
+</style>
---
+++ client/src/components/BookingsForm.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+	<form id="bookings-form" v-on:submit.prevent="addBooking" v-model="newBooking">
+		<h2>Create a New Booking</h2>
+		<div class="formWrap">
+			<label for="name">Full Name:</label>
+			<input v-model="newBooking.name" type="text" id="name" required />
+		</div>
+		<div class="formWrap">
+			<label for="email">Email Adress:</label>
+			<input v-model="newBooking.email" type="text" id="email" required />
+		</div>
+		<div class="formWrap">
+			<label for="checked">Checked in:</label><br>
+      <input v-model="newBooking.check" type="checkbox" name="yes" value=true> Yes
+      <input v-model="newBooking.check" type="checkbox" name="no" value=false> No
+		</div>
+
+		<input type="submit" value="Save" id="save"/>
+	</form>
+</template>
+
+<script>
+import { eventBus } from '../main.js'
+export default {
+	name: "BookingsForm",
+	data(){
+		return {
+			newBooking: {
+				name: "",
+				email: "",
+				cheked: null
+			}
+		}
+	},
+	methods: {
+		addBooking(){
+			fetch('http://localhost:3000/api/bookings', {
+				method: 'POST',
+				body: JSON.stringify(this.newBooking),
+				headers: { 'Content-Type': 'application/json' }
+			})
+			.then(response => response.json())
+			.then(data => eventBus.$emit('booking-added', data))
+		}
+	}
+}
+</script>
